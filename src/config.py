8e35453746a414db1db711b52cb025267e0f1a9a
moyanxinxu <wwxy.mail@gmail.com>
2025-07-11 06:01:37 +0000

    feat: 更新主入口程序路径，修改化学物质知识检索工具描述，优化模型调用逻辑

--- src/config.py
+++ src/config.py
@@ -8,7 +8,8 @@ class hp:
     ollama_embedding_model: str = "nomic-embed-text:latest"
 
     siliconflow_base_url: str = "https://api.siliconflow.cn/v1"
-    siliconflow_chat_model: str = "deepseek-ai/DeepSeek-R1-Distill-Qwen-7B"
+    # siliconflow_chat_model: str = "deepseek-ai/DeepSeek-R1-Distill-Qwen-7B"
+    siliconflow_chat_model: str = "Qwen/Qwen3-8B"
     siliconflow_embedding_model: str = "BAAI/bge-large-zh-v1.5"
 
     max_batch_size: int = 32
--- src/core/agent.py
+++ src/core/agent.py
@@ -1,3 +1,4 @@
+from langchain_core.messages.base import BaseMessage
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.graph import MessagesState, StateGraph
 from langgraph.graph.state import CompiledStateGraph
@@ -5,15 +6,16 @@ from langgraph.prebuilt import ToolNode, tools_condition
 
 from src.core import ToolSet
 from src.db import FaissDB
-from src.model import OllamaClient
+from src.model import OllamaClient, SiliconflowClient
 
-chat_model = OllamaClient().get_chat_model()
+chat_model = SiliconflowClient().get_chat_model()
 embed_model = OllamaClient().get_embed_model()
 
 
 def get_graph(tools: list) -> CompiledStateGraph:
-    def call_model(state):
-        llm_with_tool = chat_model.bind_tools(tools, tool_choice="any")
+    llm_with_tool = chat_model.bind_tools(tools)
+
+    def call_model(state) -> dict[str, BaseMessage]:
         return {"messages": llm_with_tool.invoke(state["messages"])}
 
     app = StateGraph(MessagesState)
@@ -36,19 +38,25 @@ db = FaissDB(db_path="/root/Documents/msds-qa/kb", embed_model=embed_model)
 tools = [
     ToolSet.get_retriever_tool(
         db=db.get_db(),
-        name="异丁烷知识问答助手",
-        description="能够回答异丁烷的性质、用途、危害和急救措施等问题。",
+        name="chemical_msds_retriever",
+        description="用于回答各种化学物质知识的工具",
     ),
 ]
 
 
 graph = get_graph(tools)
-
-for event in graph.invoke(
-    {"messages": "调用工具查询：氢化钙泄漏的时候应该怎么进行急救措施？"},
+out = graph.invoke(
+    {
+        "messages": [
+            {
+                "role": "system",
+                "content": "你是一个有帮助的问答机器人，你拥有很多工具可以使用，你需要根据用户的提问来决定是否调用工具，自主进行回答用户的问题，必要时可以调用工具获取额外的信息。",
+            },
+            {
+                "role": "human",
+                "content": "1,1-二甲基环己烷与火反应会生成某些有害气体，请问这些气体有哪些？",
+            },
+        ]
+    },
     config={"configurable": {"thread_id": 42}},
-    # stream_mode="messages",
-    debug=False,
-):
-    # print(event)
-    pass
+)
